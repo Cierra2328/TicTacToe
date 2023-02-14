a5c87c676d35d05b4fa1143a9159bf2d4521c821
Cierra2328 <ap28sooners@gmail.com>
2023-02-13 20:53:42 -0600

    game is finished

--- tic tac toe.py
+++ tic tac toe.py
@@ -1,25 +1,22 @@
 from tkinter import *
+from tkinter import messagebox
 
 root = Tk()
 
 root.title("Tic Tac Toe")
-clicked = True
 count = 0
 def click(b):
-    global clicked, count
-    if clicked == True and b["text"] == " ":
+    global count
+    if count %2 == 0 and b["text"] == " ":
         b["text"] = "X"
-        clicked = False
         count+=1
-    elif clicked == False and b["text"] == " ":
+        win()
+    elif b["text"] == " ":
         b["text"] = "O"
-        clicked = True
         count +=1
+        win()
     else:
         messagebox.showerror("Tic Tac Toe", "Box has already been checked")
-
-def win():
-    for 
 
 b1 = Button(root, font = (20), height = 3, width = 7, text = " ", command = lambda: click(b1))
 b2 = Button(root,font = (20), height = 3, width = 7, text = " ", command = lambda: click(b2))
@@ -44,3 +41,119 @@ b6.grid(row=1, column = 2)
 b7.grid(row=2, column = 0)
 b8.grid(row=2, column = 1)
 b9.grid(row=2, column = 2)
+
+def disableButtons():
+    b1.config(state = DISABLED)
+    b2.config(state = DISABLED)
+    b3.config(state = DISABLED)
+    b4.config(state = DISABLED)
+    b5.config(state = DISABLED)
+    b6.config(state = DISABLED)
+    b7.config(state = DISABLED)
+    b8.config(state = DISABLED)
+    b9.config(state = DISABLED)
+    
+def win():
+    if b1["text"] == "X" and b2["text"] == "X" and b3["text"] == "X":
+        b1.config(bg="red") 
+        b2.config(bg="red")
+        b3.config(bg="red")
+        disableButtons()
+        messagebox.info("Tic Tac Toe", "X Wins")
+    elif b4["text"] == "X" and b5["text"] == "X" and b6["text"] == "X":
+        b4.config(bg="red")
+        b5.config(bg="red")
+        b6.config(bg="red")
+        disableButtons()
+        messagebox.info("Tic Tac Toe", "X Wins")    
+    elif b7["text"] == "X" and b8["text"] == "X" and b9["text"] == "X":
+        b7.config(bg = "red") 
+        b8.config(bg = "red")
+        b9.config(bg = "red")
+        disableButtons()
+        messagebox.info("Tic Tac Toe", "X Wins")    
+    elif b1["text"] == "X" and b4["text"] == "X" and b7["text"] == "X":
+        b4.config(bg = "red") 
+        b1.config(bg = "red")
+        b7.config(bg = "red")
+        disableButtons()
+        messagebox.info("Tic Tac Toe", "X Wins")
+    elif b2["text"] == "X" and b5["text"] == "X" and b8["text"] == "X":
+        b2.config(bg = "red") 
+        b5.config(bg = "red")
+        b8.config(bg = "red")
+        disableButtons()
+        messagebox.showinfo("Tic Tac Toe", "X Wins")
+    elif b3["text"] == "X" and b6["text"] == "X" and b9["text"] == "X":
+        b3.config(bg = "red") 
+        b6.config(bg = "red")
+        b9.config(bg = "red")
+        disableButtons()
+        messagebox.showinfo("Tic Tac Toe", "X Wins")
+    elif b1["text"] == "X" and b5["text"] == "X" and b9["text"] == "X":
+        b1.config(bg = "red") 
+        b5.config(bg = "red")
+        b9.config(bg = "red")
+        disableButtons()
+        messagebox.showinfo("Tic Tac Toe", "X Wins")
+    elif b3["text"] == "X" and b5["text"] == "X" and b7["text"] == "X":
+        b3.config(bg = "red") 
+        b5.config(bg = "red")
+        b7.config(bg = "red")
+        disableButtons()
+        messagebox.showinfo("Tic Tac Toe", "X Wins")
+    elif b1["text"] == "O" and b2["text"] == "O" and b3["text"] == "O":
+        b1.config(bg = "red") 
+        b2.config(bg = "red")
+        b3.config(bg = "red")
+        disableButtons()
+        messagebox.showinfo("Tic Tac Toe", "X Wins")
+    elif b4["text"] == "O" and b5["text"] == "O" and b6["text"] == "O":
+        b4.config(bg = "red") 
+        b5.config(bg = "red")
+        b6.config(bg = "red")
+        disableButtons()
+        messagebox.showinfo("Tic Tac Toe", "X Wins")
+    elif b7["text"] == "O" and b8["text"] == "O" and b9["text"] == "O":
+        b7.config(bg = "red") 
+        b8.config(bg = "red")
+        b9.config(bg = "red")
+        disableButtons()
+        messagebox.showinfo("Tic Tac Toe", "X Wins")
+    elif b1["text"] == "O" and b4["text"] == "O" and b7["text"] == "O":
+        b4.config(bg = "red") 
+        b1.config(bg = "red")
+        b7.config(bg = "red")
+        disableButtons()
+        messagebox.showinfo("Tic Tac Toe", "X Wins")
+    elif b2["text"] == "O" and b5["text"] == "O" and b8["text"] == "O":
+        b2.config(bg = "red") 
+        b5.config(bg = "red")
+        b8.config(bg = "red")
+        disableButtons()
+        messagebox.showinfo("Tic Tac Toe", "X Wins")
+    elif b3["text"] == "O" and b6["text"] == "O" and b9["text"] == "O":
+        b3.config(bg = "red") 
+        b6.config(bg = "red")
+        b9.config(bg = "red")
+        disableButtons()
+        messagebox.showinfo("Tic Tac Toe", "X Wins")
+    elif b1["text"] == "O" and b5["text"] == "O" and b9["text"] == "O":
+        b1.config(bg = "red") 
+        b5.config(bg = "red")
+        b9.config(bg = "red")
+        disableButtons()
+        messagebox.showinfo("Tic Tac Toe", "X Wins")
+    elif b3["text"] == "O" and b5["text"] == "O" and b7["text"] == "O":
+        b3.config(bg = "red") 
+        b5.config(bg = "red")
+        b7.config(bg = "red")
+        disableButtons()
+        messagebox.showinfo("Tic Tac Toe", "X Wins")
+
+
+
+
+
+
+    
